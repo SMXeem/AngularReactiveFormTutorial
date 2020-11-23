76f5d0a60cb581cb4909932e3c44a8485b84d282
Sayed Al Zawad XEEM <sayedxeem24@gmail.com>
2020-11-23 16:11:26 +0600

    Nesting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,10 @@ export class AppComponent {
   registrationForm=new FormGroup({
     userName:new FormControl('Sayed'),
     password:new FormControl(''),
-    confirmPassword:new FormControl('')
+    confirmPassword:new FormControl(''),
+    address:new FormGroup({
+      city:new FormControl(''),
+      state:new FormControl(''),
+    })
   });
 }
